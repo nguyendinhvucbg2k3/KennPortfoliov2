7fa572a3dcec1c16c5e820a2db272b5f817f36ea
Thac Nguyen Dinh Vu - Kenn ED (Kenn Design) <thacnguyendinhvu.esports@gmail.com>
2025-10-20 22:05:07 +0000

    dùng cái này đi

--- src/components/icons/NeonBloomLogo.tsx
+++ src/components/icons/NeonBloomLogo.tsx
@@ -7,12 +7,10 @@ const KdLogoSvg = ({ className }: { className?: string }) => (
     fill="currentColor"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path
-      d="M10 10 L 10 90 L 30 90 L 30 60 L 50 90 L 70 90 L 40 50 L 70 10 L 50 10 L 30 40 L 30 10 L 10 10 Z"
-    />
-    <path
-      d="M60 10 C 85 10, 95 20, 95 50 C 95 80, 85 90, 60 90 L 60 70 C 75 70, 80 65, 80 50 C 80 35, 75 30, 60 30 L 60 10 Z"
-    />
+    {/* Letter K */}
+    <path d="M10 10 L 10 90 L 30 90 L 30 55 L 55 90 L 70 90 L 40 50 L 70 10 L 55 10 L 30 45 L 30 10 L 10 10 Z" />
+    {/* Letter D */}
+    <path d="M60 10 C 85 10, 95 25, 95 50 C 95 75, 85 90, 60 90 L 60 10 Z M 75 30 L 75 70 C 80 65, 80 35, 75 30 Z" />
   </svg>
 );
 
