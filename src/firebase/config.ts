481110b35f641b8dd96d51873b58854683cacf41
Thac Nguyen Dinh Vu - Kenn ED (Kenn Design) <thacnguyendinhvu.esports@gmail.com>
2025-10-20 20:15:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8961062049-3891e",
+  "appId": "1:432834151777:web:b33a908155b0e82e813120",
+  "apiKey": "AIzaSyC09goRsvU3iPK9-MTZnpfCV6jBTykuxAk",
+  "authDomain": "studio-8961062049-3891e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "432834151777"
+};
