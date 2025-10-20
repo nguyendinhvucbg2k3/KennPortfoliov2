83d3031b9d56bd487f8b3747dd8ec536df94168f
Thac Nguyen Dinh Vu - Kenn ED (Kenn Design) <thacnguyendinhvu.esports@gmail.com>
2025-10-20 22:06:28 +0000

    dùng logo trong file public

--- src/components/icons/NeonBloomLogo.tsx
+++ src/components/icons/NeonBloomLogo.tsx
@@ -8,9 +8,10 @@ const KdLogoSvg = ({ className }: { className?: string }) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     {/* Letter K */}
-    <path d="M10 10 L 10 90 L 30 90 L 30 55 L 55 90 L 70 90 L 40 50 L 70 10 L 55 10 L 30 45 L 30 10 L 10 10 Z" />
+    <path d="M10 10 H 30 V 45 L 55 10 H 70 L 40 50 L 70 90 H 55 L 30 55 V 90 H 10 V 10 Z" />
+    
     {/* Letter D */}
-    <path d="M60 10 C 85 10, 95 25, 95 50 C 95 75, 85 90, 60 90 L 60 10 Z M 75 30 L 75 70 C 80 65, 80 35, 75 30 Z" />
+    <path d="M60 10 H 75 C 90 10, 95 25, 95 50 C 95 75, 90 90, 75 90 H 60 V 10 Z M 75 30 V 70 C 80 65, 80 35, 75 30 Z" />
   </svg>
 );
 
