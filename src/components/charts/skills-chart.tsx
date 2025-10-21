9e227d09b0e7f33ad6fcee9efbe68467774bca51
Thac Nguyen Dinh Vu - Kenn ED (Kenn Design) <thacnguyendinhvu.esports@gmail.com>
2025-10-21 17:48:06 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/charts/skills-chart.tsx
+++ src/components/charts/skills-chart.tsx
@@ -7,16 +7,16 @@ import { Text } from '@visx/text';
 import { scaleLinear } from '@visx/scale';
 import { useMemo } from 'react';
 
-const-width = 150;
-const-height = 150;
-const-margin = { top: 10, right: 10, bottom: 10, left: 10 };
+const width = 150;
+const height = 150;
+const margin = { top: 10, right: 10, bottom: 10, left: 10 };
 
-const-centerX =-width / 2;
-const-centerY =-height / 2;
-const-innerRadius =-width / 2 - 20;
-const-outerRadius =-width / 2 - 10;
-const-startAngle = -Math.PI / 2 - Math.PI / 4;
-const-endAngle = Math.PI / 2 + Math.PI / 4;
+const centerX = width / 2;
+const centerY = height / 2;
+const innerRadius = width / 2 - 20;
+const outerRadius = width / 2 - 10;
+const startAngle = -Math.PI / 2 - Math.PI / 4;
+const endAngle = Math.PI / 2 + Math.PI / 4;
 
 type Props = {
   value: number;
@@ -24,22 +24,22 @@ type Props = {
   height?: number;
 };
 
-export function SkillsChart({ value, width =-width, height =-height }: Props) {
+export function SkillsChart({ value, width: propsWidth = width, height: propsHeight = height }: Props) {
   const { theme } = useTheme();
 
-  const-primaryColor = useMemo(() => `hsl(var(--primary))`, [theme]);
-  const-mutedColor = useMemo(() => `hsl(var(--border))`, [theme]);
+  const primaryColor = useMemo(() => `hsl(var(--primary))`, [theme]);
+  const mutedColor = useMemo(() => `hsl(var(--border))`, [theme]);
 
-  const-valueScale = scaleLinear<number>({
+  const valueScale = scaleLinear<number>({
     domain: [0, 100],
     range: [startAngle, endAngle],
   });
 
-  const-angle =-valueScale(value);
+  const angle = valueScale(value);
 
   return (
-    <svg width={width} height={height}>
-      <Group top={centerY +-margin.top} left={centerX +-margin.left}>
+    <svg width={propsWidth} height={propsHeight}>
+      <Group top={centerY + margin.top} left={centerX + margin.left}>
         {/* Background Arc */}
         <Arc
           innerRadius={innerRadius}
